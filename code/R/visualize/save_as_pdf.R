dbad5d916392f84e481d20fcee78df93e18be0b6
YJLee <ylee14@uw.edu>
2025-02-19 13:15:49 -0800

    change DPI from 300 to 1,000

--- code/R/visualize/save_as_pdf.R
+++ code/R/visualize/save_as_pdf.R
@@ -5,9 +5,9 @@ save_as_pdf <- function(filename, width=width_inches) {
   img <- image_read(paste0(filename, ".png"))
   
   # Calculate the width in pixels (assuming 300 DPI)
-  width_pixels <- width * 300
+  width_pixels <- width * 1000
   
   # Resize the image
   img_resized <- image_scale(img, paste0(width_pixels, "x"))
-  image_write(img_resized, paste0(filename, ".pdf"), format="pdf", density=300)
+  image_write(img_resized, paste0(filename, ".pdf"), format="pdf", density=1000)
 }
